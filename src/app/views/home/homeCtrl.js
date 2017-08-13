5ae91b86d0981427aa604a77a86e1f11f2ec6a5b
Stefan Blattmann <stefan.blattmann@gmail.com>
2017-08-13 14:09:56 +0200

    home component updated

--- src/app/views/home/homeCtrl.js
+++ src/app/views/home/homeCtrl.js
@@ -1,18 +1,39 @@
 import style from './homeView.scss';
 
 export default function ($scope, $http, CONFIG) {
-  console.log('homeCtrl.js');
+  // Declare some vars.
   let self = this;
   let source = CONFIG.dataUrl;
 
+  // Apply custom view styles.
   $scope.style = style;
+
+  // Hide details on init.
   $scope.showParking = false;
 
+  // Toggle button class.
+  $scope.className = "home__button--active";
+  $scope.changeClass = function(){
+    if ($scope.className === "home__button--active")
+      $scope.className = "home__button--inactive";
+    else
+      $scope.className = "home__button--active";
+  };
+
+  // Get the data from the API.
   $http.get(source)
     .then(function(res) {
-      // return the enveloped data
-      console.log('res: ', res);
+      // Return the data.
       self.name = res.data.vendor.name;
+      self.departureAirport = res.data.vendor.departureAirport;
       self.map = res.data.vendor.map;
+      self.features = res.data.vendor.features;
+      self.featuresTitle = res.data.dictionary.featuresTitle;
+      self.bookYourParking = res.data.dictionary.bookYourParking;
     })
+
+  // Content
+  self.show = 'Show details';
+  self.hide = 'Hide details';
+  self.logo = 'Logo';
 }
--- src/app/views/home/index.js
+++ src/app/views/home/index.js
@@ -1,11 +1,9 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
-import routing from './homeRoutes';
 import HomeController from './homeCtrl';
 
 export default angular
   .module('app.home', [uirouter])
-  .config(routing)
   .controller('HomeController', HomeController)
   .name;
