30c0a71d8f5f45ca6bbcee76ab7e37e80abe5665
Stefan Blattmann <stefan.blattmann@gmail.com>
2017-08-13 17:05:37 +0200

    global styles injected in header controller

--- src/app/views/header/headerCtrl.js
+++ src/app/views/header/headerCtrl.js
@@ -1,7 +1,11 @@
+import globalstyle from '../../../style/global.scss';
 import style from './headerView.scss';
 
 header.$inject = ['$scope'];
 export default function header($scope) {
+  // Apply custom global styles.
+  $scope.style = globalstyle;
+
   // Apply custom view styles.
   $scope.style = style;
 
