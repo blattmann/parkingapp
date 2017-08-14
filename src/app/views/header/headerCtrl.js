dd0910f88b5672851a9bbf3a85bc32b148623089
Stefan Blattmann <stefan.blattmann@gmail.com>
2017-08-14 10:43:24 +0200

    header component modified

--- src/app/views/header/headerCtrl.js
+++ src/app/views/header/headerCtrl.js
@@ -1,14 +1,22 @@
 import globalstyle from '../../../style/global.scss';
 import style from './headerView.scss';
 
-header.$inject = ['$scope'];
-export default function header($scope) {
+header.$inject = ['$scope', '$http', 'CONFIG'];
+export default function header($scope, $http, CONFIG) {
+  // Declare some vars.
+  let self = this;
+  let source = CONFIG.dataUrl;
+
   // Apply custom global styles.
   $scope.style = globalstyle;
 
   // Apply custom view styles.
   $scope.style = style;
 
-  // Content
-  $scope.headline = 'My Parking App';
+  // Get the data from the API.
+  $http.get(source)
+    .then(function(res) {
+      // Return the data.
+      self.headline = res.data.vendor.name;
+    })
 }
